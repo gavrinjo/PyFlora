d787af057b7c3f20766da073121d234077f839cd
Dražen Gavrić <drazen.gavric@gmail.com>
2023-08-01 15:20:12 +0200

    charts update

--- app/repo.py
+++ app/repo.py
@@ -4,55 +4,14 @@ import base64
 from matplotlib.figure import Figure
 import numpy as np
 
-# Generate the figure **without using pyplot**.
-fig = Figure()
-ax = fig.subplots()
-x = [1, 2, 3]
-y = np.array([[1, 2], [3, 4], [5, 6]])
-ax.plot(x,y)
-# Save it to a temporary buffer.
-# buf = io.BytesIO()
-# fig.savefig(buf, format="png")
-# # Embed the result in the html output.
-# data = base64.b64encode(buf.getbuffer()).decode("ascii")
-# fig.savefig('test.png', format='png')
-
 
 class PyGraf(Figure):
 
-    def __init__(self, ax1, ax2, *args, **kwargs) -> None:
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.ax1 = ax1
-        self.ax2 = ax2
-        
 
-    def _subs(self):
-
-        ax = self.subplots()
-        # ax.grid(True, linestyle=':', linewidth=1)
-        # ax.hist(self.ax1, bins=5, label='test', edgecolor='black') # hist chart
-        # ax.pie(self.ax1) # pie chart
-        ax.plot(self.ax1, self.ax2, label='test') # line chart
-        ax.legend()
-
-    def radar_chart(self):
-        num_vars = len(self.ax1)
-        values = self.ax2
-        angles = np.linspace(0, 2 * np.pi, num_vars, endpoint=False).tolist()
-        values += values[:1]
-        angles += angles[:1]
-        ax = self.subplots(subplot_kw=dict(polar=True))
-        ax.set_theta_offset(np.pi /2)
-        ax.set_theta_direction(-1)
-        ax.set_thetagrids(np.degrees(angles[:-1]), self.ax1)
-        ax.set_ylim(0, 16)
-        ax.set_rlabel_position(180 / num_vars)
-        ax.plot(angles, values, color='red', linewidth=1)
-        ax.fill(angles, values, color='red', alpha=0.25)
-
-
-    def _save(self):
-        self.savefig('bar_01.png', format='png')
+    def _save(self, file_name):
+        self.savefig(f'{file_name}.png', format='png')
 
     def represent_chart(self):
         buf = io.BytesIO()
@@ -60,14 +19,92 @@ class PyGraf(Figure):
         return base64.b64encode(buf.getbuffer()).decode("ascii")
 
 
+class Line(PyGraf):
 
-ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.set_tight_layout(True)
+        self.ax = self.subplots()
+    
+    def plot(self, x_axis, y_axis, color, label):
+        self.ax.grid(True)
+        self.ax.plot(x_axis, y_axis, marker='.' , color=color, label=label) # line chart
+        # self.ax.xaxis.set_ticks(x_axis)
+        # self.ax.yaxis.set_ticks(y_axis)
+        # self.ax.margins(x=0, y=0)
+        self.ax.legend()
 
-dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
 
+class Histogram(PyGraf):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.set_tight_layout(True)
+        self.ax = self.subplots()
+    
+    def plot(self, values, bins, label):
+        self.ax.grid(True, linestyle=':', linewidth=1)
+        self.ax.hist(values, bins=bins, label=label, edgecolor='black') # hist chart
+        self.ax.axvline(29, color='red', label='avg medan')
+        # self.ax.margins(x=0, y=0)
+        self.ax.legend()
+
+
+class Radar(PyGraf):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.ax = self.subplots(subplot_kw=dict(polar=True))
+        # self.angles = np.linspace(0, 2 * np.pi, N, endpoint=False).tolist()
+    
+
+    def plot(self, labels, values, color, text):
+        
+        N = len(labels)
+        values = values
+        angles = np.linspace(0, 2 * np.pi, N, endpoint=False).tolist()
+        values += values[:1]
+        angles += angles[:1]
+        # ax = self.subplots(subplot_kw=dict(polar=True))
+        self.ax.set_theta_offset(np.pi / 2)
+        self.ax.set_theta_direction(-1)
+        self.ax.set_thetagrids(np.degrees(angles[:-1]), labels, size=10, fontweight="bold")
+        self.ax.tick_params(pad=10, grid_linestyle=':')
+        self.ax.set_rticks(range(0, 16, 2), range(0, 16, 2), color="grey", size=8, fontweight="bold")
+        self.ax.set_rlim(bottom=0, top=16)
+        self.ax.set_rlabel_position(180 / N)
+        self.ax.plot(angles, values, color=color, linewidth=1, label=text)
+        self.ax.fill(angles, values, color=color, alpha=0.25)
+        self.ax.legend(loc='upper right', bbox_to_anchor=(1.3, 1.1))
+
+
+
+# histogram plot
+ages = [18, 19, 21, 25, 26, 26, 30, 32, 38, 45, 55]
+bins = range(10, 61, 10)
+
+
+# radar plot
 employee = ["Sam", "Rony", "Albert", "Chris", "Jahrum"]
 actual = [7, 3, 5, 6, 14]
+actual2 = [3, 4, 2, 8, 5]
 
-graf = PyGraf(employee, actual)
-graf.radar_chart()
-graf._save()
+# line plot
+x = np.arange(0, 10)
+y1 = np.array([r**2 for r in x])
+y2 = np.array([r**3 for r in x])
+
+
+chart = Radar()
+chart.plot(employee, actual, 'red', 'test1')
+chart.plot(employee, actual2, 'blue', 'test2')
+chart._save('test2')
+
+# lchar = Line()
+# lchar.plot(x, y1, 'blue', 'test')
+# lchar.plot(x, y2, 'red', 'test2')
+# lchar._save('test_03')
+
+# hchart = Histogram()
+# hchart.plot(ages, bins, 'test')
+# hchart._save('hist_02')
